1c3e60597bd084ccf2db6f56b5f515f7a8ddb7f4
VovaTS <77009972+VovaTS@users.noreply.github.com>
2024-04-06 10:54:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,11 +8,22 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
 ThemeToggle()    // dark mode
 }
 
-let x = 51
-// if (x==50){
-// console.log(".")
-// }
-// else {
-//     console.log("..")
-// }
-console.log(x==51)
+let sendb = document.querySelector("#sendbutton")
+sendb.addEventListener("click", async function(e){
+    e.preventDefault()
+    let name = document.querySelector("#username")
+    let number = document.querySelector("#phonenumber")
+    let textarea = document.querySelector("#userwish")
+    let token = "6036086539:AAFPhEDtOxxFv5gdjEl0g_mw6eSE7k557is"
+    let id = "2092827728"
+    let data =` 
+    Новый Заказ:%0A
+    Имя: ${name.value}%0A
+    Номер: ${number.value}%0A
+    Пожелание: ${textarea.value}%0A
+    `
+    let url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${id}&text=${data}`
+    let response = await fetch(url)
+    let result = await response.json()
+    console.log(result)
+})
